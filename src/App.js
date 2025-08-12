9165a94b11a66861844c2277f0f966eb46dbe03e
otorquati <torquati.osvaldo@gmail.com>
2025-08-11 21:08:33 -0300

    feat: tutorial 13 - fetch data from API

--- src/App.js
+++ src/App.js
@@ -6,13 +6,34 @@ import { useEffect, useState } from "react";
 import SearchItem from "./Components/SearchItem";
 
 function App() {
-  const [items, setItems] = useState(JSON.parse(localStorage.getItem("shoppinglist")) || []);
+  const API_URL = 'http://localhost:3500/items';
+
+  const [items, setItems] = useState([]);
   const [newItem, setNewItem] = useState("");
   const [searchItem, setSearchItem] = useState("");
+  const [fetchError, setFetchError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    localStorage.setItem("shoppinglist", JSON.stringify(items));
-  },[items]);
+    
+    const fetchItems = async () => {
+      try {
+        const response = await fetch(API_URL);
+        if (!response.ok) throw Error("Did not receive expected data");
+        const listItems = await response.json();
+        setItems(listItems);
+        setFetchError(null);
+      } catch (err) {
+        setFetchError(err.message);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    setTimeout(() => {
+      (async () => await fetchItems())();
+    }, 10000);
+
+  },[]);
 
   // Function to add a new item
   const addItem = (item) => {
@@ -51,14 +72,18 @@ function App() {
         handleSubmit={handleSubmit}
       />
       <SearchItem searchItem={searchItem} setSearchItem={setSearchItem} />
-      <Content
+      <main>
+        {isLoading && <p style={{ marginTop: "2rem" }}>Loading items...</p>}
+        {fetchError && <p style={{ color: "red" }}>{`Error: ${fetchError}`}</p>}
+        { !fetchError && !isLoading && <Content
         items={items.filter((item) =>
           ((item.item).toLowerCase()).includes(searchItem.toLowerCase())
         )}
         setItems={setItems}
         handleCheck={handleCheck}
         handleDelete={handleDelete}
-      />
+      />}
+      </main>
       <Footer length={items.length} />
     </div>
   );
